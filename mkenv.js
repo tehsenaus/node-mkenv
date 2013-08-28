9510c16eb2dba40ae751b6fcbedf3759782053d3
unknown <sean@sdmworld.co.uk>
2013-08-28 19:24:19 +0100

    Fixes, New Features, Docs

--- mkenv.js
+++ mkenv.js
@@ -5,8 +5,11 @@ module.exports = function () {
 	function eval(name) {
 		var value = null;
 		for ( var i = chain.length - 1; value === null && i >= 0; i-- ) {
-			if ( chain[i].hasOwnProperty(name) ) {
-				value = chain[i][name];
+			var env = chain[i];
+			if ( typeof env === "function" && env !== eval ) {
+				value = env(name);
+			} else if ( env && env.hasOwnProperty(name) ) {
+				value = env[name];
 			}
 		}
 
@@ -32,5 +35,42 @@ module.exports = function () {
 		return value;
 	}
 
+	// Allow overriding variables by assignment
+	chain.push(eval);
+
+	Object.defineProperty(eval, '__mkenv_chain', {
+		enumerable: false,
+		value: chain
+	});
+
 	return eval;
+}
+
+function getKeys(env) {
+	var chain = env.__mkenv_chain;
+	if (!chain) return [];
+
+	var keys = {};
+	chain.forEach(function (item) {
+		if ( typeof item === "function" && item !== env ) {
+			getKeys(item).forEach(function (key) {
+				keys[key] = true;
+			})
+		} else if (item) {
+			Object.keys(item).forEach(function (key) {
+				keys[key] = true;
+			})
+		}
+	})
+
+	return Object.keys(keys);
+}
+module.exports.keys = getKeys;
+
+module.exports.vars = function vars(env) {
+	var vars = {};
+	getKeys(env).forEach(function (key) {
+		vars[key] = env(key);
+	});
+	return vars;
 }
